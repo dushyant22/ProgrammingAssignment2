81653a48b164aed4cf194b8bce424cd10bf8f858
dushyant22 <dushy_ant@hotmail.com>
2017-11-04 07:23:54 +0530

    Variable name
    Customised as per my exercise offline.

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,25 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(aMatrix = matrix()) {
+  m <- NULL 
+  set <- function(y) {
+    aMatrix <<- y
+    m <<- NULL
+  }
+  get <- function() 
+  {
+    aMatrix
+  }
+  setInverted <- function(Inverted) 
+  {
+    m <<- Inverted 
+  }
+  getInverted <- function()  m 
+  list(set = set, get = get,
+       setInverted = setInverted,
+       getInverted = getInverted)
+  
 }
 
 
